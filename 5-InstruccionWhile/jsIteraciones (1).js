764e08ab9039722addb201dff64a12f181ca87f5
BaduSC19 <54905689+BaduSC19@users.noreply.github.com>
2019-09-11 16:43:21 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,6 +1,24 @@
 function mostrar()
 {
-	alert('iteración while');
-
-
+	var contador = 0;
+	while (contador < 10){
+		contador++;
+		console.log("Cont:" + contador);
+		
+	
+		
+	}
+	console.log("Afuera cont:" + contador);
 }//FIN DE LA FUNCIÓN
+
+
+/*
+var contador = 0;
+while(contador <10)´
+if (!(contador % 2))
+{
+	continue;
+}
+console.log(contador);
+contador++;
+*/
